5d64324d36559238cba8856226fb13999ab1d4cc
Langmesturg <max.maximv6@gmail.com>
2024-11-27 12:47:39 +0200

    lab_3

---
+++ src/main/java/lab_3/Student.java
@@ -0,0 +1,62 @@
+package lab_3;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Student implements Comparable<Student> {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String recordBookNumber;
+
+    public Student(String firstName, String lastName, LocalDate birthDate, String recordBookNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.recordBookNumber = recordBookNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getRecordBookNumber() {
+        return recordBookNumber;
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        return this.lastName.compareTo(other.lastName);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(recordBookNumber, student.recordBookNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(recordBookNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDate=" + birthDate +
+                ", recordBookNumber='" + recordBookNumber + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/lab_3/StudentService.java
@@ -0,0 +1,26 @@
+package lab_3;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StudentService {
+
+    // Метод для фільтрації студентів за прізвищем
+    public List<Student> filterByLastName(List<Student> students, String lastName) {
+        return students.stream()
+                .filter(student -> student.getLastName().equalsIgnoreCase(lastName))
+                .collect(Collectors.toList());
+    }
+
+    // Метод для сортування студентів за датою народження (Comparator)
+    public List<Student> sortByBirthDate(List<Student> students) {
+        return students.stream()
+                .sorted(Comparator.comparing(Student::getBirthDate))
+                .collect(Collectors.toList());
+    }
+
+    // Метод для сортування студентів за прізвищем (Comparable)
+    public List<Student> sortByLastName(List<Student> students) {
+        Collections.sort(students);
+        return students;
+    }
+}
